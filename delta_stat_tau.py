ca7d011d826d31e8fd46655eca0c7e7d191b9513
Andrés Yagüe López <and.yague@gmail.com>
2020-10-02 09:47:32 +0200

    Proper values of K added to the code

--- delta_stat_tau.py
+++ delta_stat_tau.py
@@ -22,15 +22,16 @@ ESS_Cm = np.random.normal(ESS_Cm_avg, sig_Cm, n_draws)
 #ESS_I = np.random.uniform(ESS_I_avg*1/2, ESS_I_avg*2, size = n_draws)
 #ESS_Cm = np.random.uniform(ESS_Cm_avg*1/2, ESS_Cm_avg*2, size = n_draws)
 
-# K values for uranium. Actually, these values are probably wrong, so let's wait
-# until I have the good ones
+# K values for stable isotope and uranium
 #     low   best   high
-#u235  1.2   1.4    2.0
-#u238  1.5   1.9    4.1
+#stab  1.5   2.1    4.8
+#u238  1.4   1.8    3.4
+#u235  1.1   1.2    1.7
 
 # Define K array (min, best, max):
-k_I = np.array((1.6, 2.3, 5.7))
-k_Cm = np.array((1.2, 1.4, 2.0))
+k_I = np.array((1.5, 2.1, 4.8))
+k_Cm = np.array((1.1, 1.2, 1.7))
+k_Pu = np.array((1.4, 1.8, 3.4))
 
 # Parameters for deltas array
 delt0 = 70
